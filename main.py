2ab039526ab9aca6b584d8e9cc63af365f2cff67
dordep19 <dordep19@gmail.com>
2021-05-28 20:33:46 +0300

    Implement get request for gene data lookup

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import os
 import pandas as pd
-from fastapi import FastAPI
+from typing import List, Optional
+from fastapi import FastAPI, Query
 
-# Load mi-INTS-db data into a pandas dataframe
+# load mi-INTS-db data into a pandas dataframe
 df = pd.read_excel(os.environ['MI_INTS_DB_FILE'], engine='openpyxl')
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-  return "Hello, I serve mi-INTS-db data!"
+@app.get('/')
+def lookup(gene: str, columns: Optional[List[str]] = Query(None)):
+  # standard lookup using first 10 columns of gene data
+  if columns == None:
+    data = df.loc[df['Gene_name'] == gene].iloc[:, :10]
+
+    return data.to_dict('records')
+  # advanced lookup using specified columns of gene data
+  else:
+    data = df.loc[df['Gene_name'] == gene].filter(items=columns)
+
+    return data.to_dict('records')
