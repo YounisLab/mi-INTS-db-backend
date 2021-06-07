c49a2e93484ed702b98431ade9205a78aff9b60f
dordep19 <dordep19@gmail.com>
2021-06-07 17:45:52 +0300

    Return 404 if gene not found

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 from typing import List, Optional
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException
 
 # load mi-INTS-db data into a pandas dataframe
 df = pd.read_excel(os.environ['MI_INTS_DB_FILE'], engine='openpyxl')
@@ -14,9 +14,15 @@ def lookup(gene: str, columns: Optional[List[str]] = Query(None)):
   if columns == None:
     data = df.loc[df['Gene_name'] == gene].iloc[:, :10]
 
+    if data.empty:
+      raise HTTPException(status_code=404, detail='Gene not found')
+
     return data.to_dict('records')
   # advanced lookup using specified columns of gene data
   else:
     data = df.loc[df['Gene_name'] == gene].filter(items=columns)
+
+    if data.empty:
+      raise HTTPException(status_code=404, detail='Gene not found')
 
     return data.to_dict('records')
